b617df6931b544a2157bcd8a9bc60f5b5e54d40c
EthanBrockway <ebrockway01@gmail.com>
2024-03-12 17:45:17 -0600

    added basic layout of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
-import Image from "next/image"
 import styles from "./page.module.css"
-
+import Link from "next/link"
+import Image from "next/image"
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section>Section 1</section>
-      <section>Section 2</section>
+      <section className={styles.container}>
+        <div className={styles.container1}>
+          <Link href="/settings" className={styles.settingsImage}>
+            <Image
+              src="/settings.png"
+              alt="Image of cog wheel"
+              height="50"
+              width="50"
+            ></Image>
+          </Link>
+          <h1 className={styles.h1}>Welcome "username"</h1>
+          <Link className={styles.link} href="/newStage">
+            Start new Stage
+          </Link>
+          <Link className={styles.link} href="/addFurniture">
+            Add Furniture
+          </Link>
+          <Link className={styles.link} href="/inventory">
+            See Available Inventory
+          </Link>
+        </div>
+        <div className={styles.container2}>Section 2</div>
+      </section>
     </main>
   )
 }
